430628cd5e5c702ec14243c727f8fd7fa65585a4
wafiq <wafiqgithub775@gmail.com>
2024-12-06 18:17:10 +0600

    learn variable grouping & const

---
+++ 1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	/*
+		Grouping variables
+			const (
+				myName string = "John"
+				myAge  int    = 69
+			)
+			fmt.Println(myName, myAge)
+	*/
+
+	/* -------------------------------------------*/
+
+	/*
+		variable declaration
+				var myAge1  int  = 69
+				var myAge2 = 69
+				myAge3:=69
+	*/
+}
