31a8f842c7a371328092453d192b5ba470ea6786
wafiq <wafiqgithub775@gmail.com>
2024-12-17 00:39:20 +0600

    fix waitgroup example

--- wait group/practical_example/main.go
+++ wait group/practical_example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"sync"
 	"time"
 )
@@ -12,54 +13,62 @@ type SocialPost struct {
 	Comment []string
 	Friends int64
 }
-type Response struct {
-	err  error
-	data interface{}
-}
 
-func (s *SocialPost) getLikes() (int64, error) {
+func (s *SocialPost) getLikes(wg *sync.WaitGroup) (int64, error) {
 	time.Sleep(time.Second * 2)
+	wg.Done()
 	return 64, nil
 }
 
-func (s *SocialPost) getComments() ([]string, error) {
+func (s *SocialPost) getComments(wg *sync.WaitGroup) ([]string, error) {
 	time.Sleep(time.Second * 2)
+	wg.Done()
 	return []string{
 		"Fucking Hilarious",
 		"Whassup",
 	}, nil
 }
 
-func (s *SocialPost) getFriends() (int64, error) {
+func (s *SocialPost) getFriends(wg *sync.WaitGroup) (int64, error) {
 	time.Sleep(time.Second * 2)
+	wg.Done()
 	return 45, nil
 }
 
-func (s *SocialPost) handleSocialPost() (*SocialPost, error) {
-	comments, err := s.getComments()
-	if err != nil {
-		return nil, err
-	}
-	friends, err := s.getFriends()
-	if err != nil {
-		return nil, err
-	}
-	likes, err := s.getLikes()
-	if err != nil {
-		return nil, err
-	}
-	return &SocialPost{
-		Likes:   likes,
-		Id:      s.Id,
-		Comment: comments,
-		Friends: friends,
-	}, nil
+func (s *SocialPost) handleSocialPost(post *SocialPost, wg *sync.WaitGroup) {
+	go func() {
+		comments, err := s.getComments(wg)
+		post.Comment = comments
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	go func() {
+		friends, err := s.getFriends(wg)
+		post.Friends = friends
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+
+	go func() {
+		likes, err := s.getLikes(wg)
+		post.Likes = likes
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}()
+	wg.Add(3)
 }
 
 func main() {
 	var wg sync.WaitGroup
-	post := SocialPost{}
-	go post.handleSocialPost(&wg)
+	post := SocialPost{
+		Id: 1,
+	}
+	post.handleSocialPost(&post, &wg)
+
+	wg.Wait()
 	fmt.Println(post)
-	fmt.Println(wg)
 }
